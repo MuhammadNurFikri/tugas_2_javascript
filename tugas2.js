b9fc289f24fe54f24e0b2734e9917ddf8422d421
Muhammad Nur fikri <mf4853432@gmail.com>
2023-01-07 20:29:07 +0700

    Tugas 2 Javascript
    Pengumpulan Tugas Niomic

---
+++ tugas2.js
@@ -0,0 +1,16 @@
+var genap = 20;
+
+var huruf = "hai apa kabar";
+
+var nilai = 80;
+
+if (nilai < 70)
+	nilai = false;
+else
+	nilai = true;
+
+kosong = null;
+
+var gakada;
+
+console.log(genap, huruf, nilai, kosong, gakada);
